3b26c7e8fee61564735ec8c170324539385704cb
Sajjan Vaidya <sajjanrajvaidya@gmail.com>
2022-01-30 12:09:02 +0545

    feat: add Formik package

--- components/Signup.jsx
+++ components/Signup.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+const initial = {
+  name: '',
+  email: '',
+  phone: '',
+};
+
 function Signup() {
   return (
     <div className="bg-red-200 flex flex-col h-full w-full">Implement form here</div>
